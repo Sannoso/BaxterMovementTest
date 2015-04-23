05463bf02a7a003bda4c7c1f04540b16b49c7d1a
Sander Ruben <sanderruben@live.nl>
2015-04-23 16:13:41 +0200

    Name of the topic posting is: /Movetestoutput from now on previously it was still chatter, from the followed example

--- catkin_ws/src/movetest/src/movetest.cpp
+++ catkin_ws/src/movetest/src/movetest.cpp
@@ -23,7 +23,6 @@ int main(int argc, char **argv)
 	return 1;
   }
 
-
   /**
    * NodeHandle is the main access point to communications with the
    * ROS system. The first NodeHandle constructed will fully initialize
@@ -48,7 +47,7 @@ int main(int argc, char **argv)
    * than we can send them, the number here specifies how many messages to
    * buffer up before throwing some away.
    */
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("Movetestoutput", 1000);
 
   ros::Rate loop_rate(10);
 
